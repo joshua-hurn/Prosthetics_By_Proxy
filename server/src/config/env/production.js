fb4630bdaf42a1c82dd15e83808b30b4382297c3
newcreature1989 <aaronmhopkins@gmail.com>
2018-11-27 16:38:56 -0600

    updating config

--- server/src/config/env/production.js
+++ server/src/config/env/production.js
@@ -3,7 +3,14 @@
 // Also set the NODE_ENV environment property to "production" (no quotes) in AWS, Heroku, etc.
 let cfg = {
     MAILGUN_API_KEY: process.env.MAILGUN_API_KEY,
-    STRIPE_SK: process.env.STRIPE_SK
+    STRIPE_SK: process.env.STRIPE_SK,
+    mysql: {
+        connectionLimit: 10,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        host: process.env.DB_HOST,
+        database: process.env.DB_SCHEMA
+    }
 };
 
 export { cfg };
