d8847d9164619d5293baca2b1e46546269fc38ca
newcreature1989 <aaronmhopkins@gmail.com>
2018-12-06 09:45:26 -0600

    committing local changes to prosthetics.js

--- client/src/services/prosthetics.js
+++ client/src/services/prosthetics.js
@@ -1,23 +1,23 @@
-import * as baseService from './base';
+import * as baseService from "./base";
 
 function all() {
-    return baseService.get('/api/prosthestics');
+  return baseService.get("/api/prosthetics");
 }
 
 function one(id) {
-    return baseService.get(`/api/prosthestics/${id}`);
+  return baseService.get(`/api/prosthetic/${id}`);
 }
 
 function insert(data) {
-    return baseService.post('/api/prosthestics', data);
+  return baseService.post("/api/prosthetics", data);
 }
 
 function update(id, data) {
-    return baseService.put(`/api/prosthestics/${id}`, data);
+  return baseService.put(`/api/prosthetic/${id}`, data);
 }
 
 function destroy(id) {
-    return baseService.destroy(`/api/prosthestics/${id}`);
+  return baseService.destroy(`/api/prosthetic/${id}`);
 }
 
 export { all, one, insert, update, destroy };
