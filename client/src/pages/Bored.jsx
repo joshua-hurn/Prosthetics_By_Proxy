aa4838308dc2cba3dce923c153ea3c3948eda02f
newcreature1989 <aaronmhopkins@gmail.com>
2018-12-10 08:47:49 -0600

    Sync

---
+++ client/src/pages/Bored.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Bored extends Component {
+    render() {
+        return (
+            <div className="Bored">
+            <input type="file" onChange=/>
+            </div>
+        );
+    }
+} 
+
+export default Bored;
