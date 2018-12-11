dd3a1f92934b255e762596e9ef31d9bc473b1347
Josh Hurn <joshua.hurn@hotmail.com>
2018-12-11 09:59:59 -0600

    pulling aarons changes

--- client/src/components/Cards/ProstheticsCard.jsx
+++ client/src/components/Cards/ProstheticsCard.jsx
@@ -4,41 +4,24 @@ import "./ProstheticsCard.css";
 
 const ProstheticsCard = props => {
   return (
-    <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+    <div className="prostheticCard col-lg-3 col-md-4 col-sm-6 col-xs-12">
       <div className="tile">
         <div className="wrapper">
-          <div className="header">Event Title</div>
-
-          <div className="banner-img">
-            <img src="http://via.placeholder.com/640x360" alt="Image 1" />
+          <div className="prostheic-img img-fluid">
+            <img className="rounded mx-auto d-block" src={props.prosthetic.prosthetic_image} alt="Image 1" />
           </div>
 
-          <div className="dates">
-            <div className="title">
-              
-              <span />
-            </div>
-            <div className="ends">
-              <strong>ENDS</strong> 14:30 JAN 2015
+          <div className="info">
+            <div className="title">{props.prosthetic.brand}</div>
+            <div className="quality">
+              <strong>Quality: </strong> {props.prosthetic.quality}
             </div>
           </div>
 
-          <div className="stats">
-            <div>
-              <strong>INVITED</strong> 3098
-            </div>
-
-            <div>
-              <strong>JOINED</strong> 562
-            </div>
-
-            <div>
-              <strong>DECLINED</strong> 182
-            </div>
-          </div>
           <Link to={`/part/${props.prosthetic.id}`} classNameName="btn">
             See More
           </Link>
+          <time class="result-date" datetime="2018-12-11 06:47" title="Tue 11 Dec 06:47:26 AM">Dec 11</time>
         </div>
       </div>
     </div>
--- client/src/pages/AllParts.jsx
+++ client/src/pages/AllParts.jsx
@@ -36,87 +36,89 @@ const renderSuggestion = suggestion => (
 let array = [{ brands: [] }, { categories: [] }];
 
 class AllParts extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        prosthetics: [],
-        displayProsthetics: [],
-        suggestions: [],
-        value: ""
-      };
-    }
-  
-    async componentDidMount() {
-      try {
-        let displayProsthetics = await prostheticsServices.all();
-        this.setState({ displayProsthetics });
-      } catch (e) {
-        console.log(e);
-      }
-      try {
-        let res = await fetch("/api/prosthetics");
-        let prosthetics = await res.json();
-        console.log(prosthetics);
-        this.setState({ prosthetics });
-      } catch (e) {
-        console.log(e);
-      }
-    }
-  
-    renderProsthetics = () => {
-      return this.state.prosthetics.map(prosthetic => {
-        return <ProstheticsCard key={prosthetic.id} prosthetic={prosthetic} />;
-      });
+  constructor(props) {
+    super(props);
+    this.state = {
+      prosthetics: [],
+      displayProsthetics: [],
+      suggestions: [],
+      value: ""
     };
-  
-    onChange = (event, { newValue }) => {
-      this.setState({
-        value: newValue
-      });
+  }
+
+  async componentDidMount() {
+    try {
+      let displayProsthetics = await prostheticsServices.all();
+      this.setState({ displayProsthetics });
+    } catch (e) {
+      console.log(e);
+    }
+    try {
+      let res = await fetch("/api/prosthetics");
+      let prosthetics = await res.json();
+      console.log(prosthetics);
+      this.setState({ prosthetics });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  renderProsthetics = () => {
+    return this.state.prosthetics.map(prosthetic => {
+      return <ProstheticsCard key={prosthetic.id} prosthetic={prosthetic} />;
+    });
+  };
+
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue
+    });
+  };
+
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
+  render() {
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: "What type of prosthetic are you looking for?",
+      value,
+      onChange: this.onChange
     };
 
-    onSuggestionsFetchRequested = ({ value }) => {
-        this.setState({
-          suggestions: getSuggestions(value)
-        });
-      };
-    
-      onSuggestionsClearRequested = () => {
-        this.setState({
-          suggestions: []
-        });
-      };
-    
-      render() {
-        const { value, suggestions } = this.state;
-        const inputProps = {
-          placeholder: "What type of prosthetic are you looking for?",
-          value,
-          onChange: this.onChange
-        };
-    
-        return (
-          <div>
-            <Navbar />
-            <Jumbotron title="Full Selection" subtitle="See What's Available" />
-            <div className="m-5">
-              <h2>Welcome to PBP</h2>
-              <p>We have a selection of prosthetics from our authorized donors.</p>
-              <Autosuggest
-                suggestions={suggestions}
-                onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-                onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-                getSuggestionValue={getSuggestionValue}
-                renderSuggestion={renderSuggestion}
-                inputProps={inputProps}
-              />
-            </div>
-            <div className="container product-container">
-              <div className="row">{this.renderProsthetics()}</div>
-            </div>
-            <Footer />
+    return (
+      <div>
+        <Navbar />
+        {/* <Jumbotron title="Full Selection" subtitle="See What's Available" /> */}
+        <div className="m-5">
+          <h2>Welcome to PBP</h2>
+          <p>We have a selection of prosthetics from our authorized donors.</p>
+          <Autosuggest
+            suggestions={suggestions}
+            onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+            onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+            getSuggestionValue={getSuggestionValue}
+            renderSuggestion={renderSuggestion}
+            inputProps={inputProps}
+          />
+        </div>
+        <div className="container product-container">
+          <div className="row">
+            {this.renderProsthetics()}
           </div>
-        );
-      }
-    }
-    export default AllParts;
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+export default AllParts;
