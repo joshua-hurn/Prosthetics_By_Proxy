9a0aac86c502e4e709fbf59026cacaa9776c76c6
newcreature1989 <aaronmhopkins@gmail.com>
2018-12-12 10:25:53 -0600

    Fixs

--- client/src/components/Cards/LowerCard.jsx
+++ client/src/components/Cards/LowerCard.jsx
@@ -5,15 +5,15 @@ import "./LowerCard.css";
 const LowerCard = props => {
   return (
     <React.Fragment>
-      <div class="row">
-        <div class="col-sm-6 mx-auto">
-          <div class="card" style={{width: '18rem', margin: '20px'}}>
-            <div class="card-body">
-              <h5 class="card-title">{props.prosthetic.brand}</h5>
-              <p class="card-text">{props.prosthetic.part}</p>
-              <p class="card-text">  length: {props.prosthetic.length}</p>
-              <p class="card-text">  width: {props.prosthetic.width}</p>
-              <p class="card-text">  weight: {props.prosthetic.weight}</p>
+      <div className="row">
+        <div className="col-sm-6 mx-auto">
+          <div className="card" style={{width: '18rem', margin: '20px'}}>
+            <div className="card-body">
+              <h5 className="card-title">{props.prosthetic.brand}</h5>
+              <p className="card-text">{props.prosthetic.part}</p>
+              <p className="card-text">  length: {props.prosthetic.length}</p>
+              <p className="card-text">  width: {props.prosthetic.width}</p>
+              <p className="card-text">  weight: {props.prosthetic.weight}</p>
               <a href="#" class="btn btn-primary">
                 View This Part
               </a>
--- client/src/components/Cards/UpperCard.jsx
+++ client/src/components/Cards/UpperCard.jsx
@@ -5,9 +5,9 @@ import "./UpperCard.css";
 const UpperCard = props => {
   return (
     <React.Fragment>
-      <div class="">
-        <div class="mx-auto" >
-          <div class="card" style={{ color: "white", width: '18rem', margin: '20px', background: "black" }}>
+      <div className="">
+        <div className="mx-auto" >
+          <div className="card" style={{ color: "white", width: '18rem', margin: '20px', background: "black" }}>
             <div className="img">
               <img
                 id="prosthetic-img"
@@ -16,13 +16,13 @@ const UpperCard = props => {
                 alt="Image 1"
               />
             </div>
-            <div class="card-body">
-              <h5 class="card-title">{props.prosthetic.brand}</h5>
-              <p class="card-text">{props.prosthetic.part}</p>
-              <p class="card-text">  length: {props.prosthetic.length}</p>
-              <p class="card-text">  width: {props.prosthetic.width}</p>
-              <p class="card-text">  weight: {props.prosthetic.weight}</p>
-              <a href="#" class="btn btn-outline-danger">
+            <div className="card-body">
+              <h5 className="card-title">{props.prosthetic.brand}</h5>
+              <p className="card-text">{props.prosthetic.part}</p>
+              <p className="card-text">  length: {props.prosthetic.length}</p>
+              <p className="card-text">  width: {props.prosthetic.width}</p>
+              <p className="card-text">  weight: {props.prosthetic.weight}</p>
+              <a href="#" className="btn btn-outline-danger">
                 View This Part
               </a>
             </div>
